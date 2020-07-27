53d8fbb71e239da92cf5549f76521d0cd57031c5
Christophe Delobel <cdelobel.ext@simplon.co>
2020-07-27 09:23:19 +0200

    update user

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
-const mess = "Hello from app.js";
-console.log(mess);
+const user = {
+  name: "Doe",
+  firstname: "John",
+  email: "john.doe@hello.bk",
+  password: "1234", // a ne pas reproduire en production !!!
+  isAdmin: true,
+};
 
-const totalCredit = document.querySelector("#totalCredit").innerText.split(" ");
-console.log(Number(totalCredit[0]));
+console.dir(user);
+
+console.log("hello John, i know your password :-)", user.password);
+
+document.querySelector("#firstname").textContent = user.firstname;
+
+// const mess = "Hello from app.js";
+// console.log(mess);
+
+// const totalCredit = document.querySelector("#totalCredit").innerText.split(" ");
+// console.log(Number(totalCredit[0]));
 //....

e45fba100b6bfc6d8219b2e953aceeb47ad0eecd
MAAOUI-CHERIF <67696078+MAAOUI-CHERIF@users.noreply.github.com>
2020-08-07 12:09:29 +0200

    presque

--- js/app.js
+++ js/app.js
@@ -2,31 +2,72 @@ let totalCredit=0;
 let totalDebit=0;
 let percentDebit1=0;
 let percentDebit2=0;
+let montant=0;
+let solde=0;
+let devise="€";
 
 let tabCredit=[
     "salaire",
     1520
 ];
 
-let tabDebit1 = [
+let tabDebit1=[
     "achat PS4",
     499.99,
-    percentDebit1
+    percentDebit1,
 ];
 
-let tabDebit2 = [
+let tabDebit2=[
     "achat TV",
     599,
-    percentDebit2
+    percentDebit2,
 ];
 
-let arrayCredit = [
+let arrayCredit=[
     tabCredit,
 ];
 
-let arrayDebit = [
+let arrayDebit=[
     tabDebit1,
-    tabDebit2
+    tabDebit2,
 ];
 
-console.log(arrayDebit);
+let bjbutton=document.getElementById("bjbutton");
+console.log(btjbutton);
+
+bjbutton.addEventListener("click", calcul);
+
+let textAera="";
+let opertaion="";
+let creditContent=document.getElementById("creditContainer");
+let debitContent=document.getElementById("debitContainer");
+
+function calcul() {
+    opertaion=document.getElementById("operation").Value;
+    textAera=document.getElementById("intitule").Value;
+    montant=document.getElementById("montant").value;
+
+    if (operation==="+"){
+        totalCredit+=montant;
+    }else{
+        totalDebit+=montant;
+    }
+    solde=totalCredit-totalDebit;    
+    console.log(solde);
+
+    if(solde> 0){
+        solde.innerHTML = `+ ${solde} ${devise}`;
+    }else{
+        document.getElementById("solde").style.color = "red";
+        solde.innerHTML = `${solde} ${devise}`;        
+    }
+    
+    
+    solde.value=`${solde} ${devise}`;
+    document.getElementById("totalCredit").textContent = `${totalCredit} ${devise}`;
+    document.getElementById("totalDebit").textContent = `${totalDebit} ${devise}`;      
+}
+calcul();
+
+ 
+ 
